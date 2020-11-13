6d6100c41e5e0154202bde12639a3a1b67c05eeb
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-11-13 13:19:42 +0600

    pomidory#21

--- js/clicks.js
+++ js/clicks.js
@@ -26,17 +26,25 @@
 
 let linkBtn = document.getElementsByClassName('categories__link');
 let arrows = document.getElementsByClassName('categories__arrow');
+let text = document.getElementsByClassName('categories__text');
+console.log(text);
 let linksArray = Array.from(linkBtn);
 let arrowsArray = Array.from(arrows);
+let textArray = Array.from(text);
 
 for (let aydai = 0; aydai < 3; aydai++) {
-  console.log('Итерация номер', aydai);
   linksArray[aydai].addEventListener('click', function (atai) {
-		atai.preventDefault();
-		console.log(atai)
-		linksArray[aydai].classList.toggle('is-active');
-		linksArray[aydai].classList.toggle('is-backward');
+    atai.preventDefault();
+    textArray[aydai].classList.toggle('is-active');
+    textArray[aydai].style.display = 'none';
+    function sultan(){
+      textArray[aydai].style.display = 'block'
+    }
+    linksArray[aydai].classList.toggle('is-active');
+    linksArray[aydai].classList.toggle('is-backward');
     arrowsArray[aydai].classList.toggle('is-active');
     arrowsArray[aydai].classList.toggle('is-backward');
+    setTimeout(sultan, 2000)
+    
   });
 }

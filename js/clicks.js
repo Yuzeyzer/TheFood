05c785eca1b259f0b17418e11fdb573570679b64
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-11-16 11:10:19 +0600

    add services

--- js/clicks.js
+++ js/clicks.js
@@ -27,24 +27,28 @@
 let linkBtn = document.getElementsByClassName('categories__link');
 let arrows = document.getElementsByClassName('categories__arrow');
 let text = document.getElementsByClassName('categories__text');
-console.log(text);
+let checkBtn = document.getElementsByClassName('categories__checkout');
 let linksArray = Array.from(linkBtn);
 let arrowsArray = Array.from(arrows);
 let textArray = Array.from(text);
+let checkBtnArray = Array.from(checkBtn);
 
 for (let aydai = 0; aydai < 3; aydai++) {
   linksArray[aydai].addEventListener('click', function (atai) {
     atai.preventDefault();
     textArray[aydai].classList.toggle('is-active');
     textArray[aydai].style.display = 'none';
-    function sultan(){
-      textArray[aydai].style.display = 'block'
+    function sultan() {
+      textArray[aydai].style.display = 'block';
+    }
+    function eraly() {
+      checkBtnArray[aydai].classList.toggle('is-active');
     }
     linksArray[aydai].classList.toggle('is-active');
     linksArray[aydai].classList.toggle('is-backward');
     arrowsArray[aydai].classList.toggle('is-active');
     arrowsArray[aydai].classList.toggle('is-backward');
     setTimeout(sultan, 2000)
-    
+    eraly()
   });
 }
